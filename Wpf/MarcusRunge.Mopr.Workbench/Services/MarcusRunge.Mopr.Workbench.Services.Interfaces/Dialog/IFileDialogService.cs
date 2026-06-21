97a4d87b6db884982a641e9322e6a9903e870670
Marcus Runge <marcus.runge@outlook.com>
2026-06-21 17:40:30 +0200

    Services und Interfaces entfernt
    Alle Service- und Interface-Klassen sowie zugehörige EventArgs und Projektdateien aus den Projekten MarcusRunge.Mopr.Workbench.Services.Interfaces und MarcusRunge.Mopr.Workbench.Services gelöscht. Damit sind sämtliche Service-Schnittstellen und deren Implementierungen nicht mehr Teil des Repositories.

--- Wpf/MarcusRunge.Mopr.Workbench/Services/MarcusRunge.Mopr.Workbench.Services.Interfaces/Dialog/IFileDialogService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MarcusRunge.Mopr.Workbench.Services.Interfaces.Dialog
-{
-    public interface IFileDialogService
-    {
-        string? SelectFolder(string title, string? initialDirectory = null);
-    }
-}
