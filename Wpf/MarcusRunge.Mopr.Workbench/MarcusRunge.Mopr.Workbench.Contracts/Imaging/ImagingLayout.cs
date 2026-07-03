3b5f90baa70e525ec292574edc5b7a4303d242ad
Marcus Runge <marcus.runge@outlook.com>
2026-07-03 16:48:37 +0200

    chore: update MOPR package dependencies
    - Add MarcusRunge.Toolbox.Localization.Wpf 1.0.3 to imaging module
    - Update MarcusRunge.Base from 1.0.1 to 1.0.2 across service projects

--- Wpf/MarcusRunge.Mopr.Workbench/MarcusRunge.Mopr.Workbench.Contracts/Imaging/ImagingLayout.cs
+++ Wpf/MarcusRunge.Mopr.Workbench/MarcusRunge.Mopr.Workbench.Contracts/Imaging/ImagingLayout.cs
@@ -1,10 +1,21 @@
-﻿namespace MarcusRunge.Mopr.Workbench.Contracts.Imaging
+﻿using MarcusRunge.Mopr.Workbench.Contracts.Properties;
+using MarcusRunge.Toolbox.Localization.Core;
+using System.ComponentModel;
+
+namespace MarcusRunge.Mopr.Workbench.Contracts.Imaging
 {
+    [TypeConverter(typeof(EnumDescriptionTypeConverter))]
     public enum ImagingLayout
     {
+        [LocalizedDescription("Viewer_Layout_Single", typeof(Resources))]
         Single = 0,
+        [LocalizedDescription("Viewer_Layout_TwoByTwo", typeof(Resources))]
         TwoByTwo = 1,
+        [LocalizedDescription("Viewer_Layout_Mpr", typeof(Resources))]
         Mpr = 2,
-        AxialSagittalCoronal = 3
+        [LocalizedDescription("Viewer_Layout_AxialSagittalCoronal", typeof(Resources))]
+        AxialSagittalCoronal = 3,
+        [LocalizedDescription("Viewer_Layout_Unknown", typeof(Resources))]
+        Unknown = 4
     }
 }
--- Wpf/MarcusRunge.Mopr.Workbench/MarcusRunge.Mopr.Workbench.Contracts/Imaging/ImagingTool.cs
+++ Wpf/MarcusRunge.Mopr.Workbench/MarcusRunge.Mopr.Workbench.Contracts/Imaging/ImagingTool.cs
@@ -1,12 +1,23 @@
-﻿namespace MarcusRunge.Mopr.Workbench.Contracts.Imaging
+﻿using MarcusRunge.Mopr.Workbench.Contracts.Properties;
+using MarcusRunge.Toolbox.Localization.Core;
+using System.ComponentModel;
+
+namespace MarcusRunge.Mopr.Workbench.Contracts.Imaging
 {
+    [TypeConverter(typeof(EnumDescriptionTypeConverter))]
     public enum ImagingTool
     {
+        [LocalizedDescription("Viewer_Tool_None", typeof(Resources))]
         None = 0,
+        [LocalizedDescription("Viewer_Tool_Zoom", typeof(Resources))]
         Zoom = 1,
+        [LocalizedDescription("Viewer_Tool_Pan", typeof(Resources))]
         Pan = 2,
+        [LocalizedDescription("Viewer_Tool_WindowLevel", typeof(Resources))]
         WindowLevel = 3,
+        [LocalizedDescription("Viewer_Tool_Crosshair", typeof(Resources))]
         Crosshair = 4,
+        [LocalizedDescription("Viewer_Tool_Measure", typeof(Resources))]
         Measure = 5
     }
 }
