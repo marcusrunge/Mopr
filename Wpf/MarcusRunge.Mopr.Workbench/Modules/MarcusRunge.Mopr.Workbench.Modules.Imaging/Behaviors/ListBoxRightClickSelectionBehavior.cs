21e65e1f2e8f84e6ea9640538bbac5e51a61139a
Marcus Runge <marcus.runge@outlook.com>
2026-07-07 19:44:08 +0200

    Improve measurement list selection styling
    - Add right-click selection behavior to the measurements list
    - Style selected and hovered measurement list items
    - Highlight selected items with themed background and border colors
    - Remove default focus visuals for cleaner properties panel UI
    - Reformat properties panel XAML for consistent indentation

---
+++ Wpf/MarcusRunge.Mopr.Workbench/Modules/MarcusRunge.Mopr.Workbench.Modules.Imaging/Behaviors/ListBoxRightClickSelectionBehavior.cs
@@ -0,0 +1,70 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace MarcusRunge.Mopr.Workbench.Modules.Imaging.Behaviors
+{
+    public static class ListBoxRightClickSelectionBehavior
+    {
+        public static readonly DependencyProperty IsEnabledProperty = DependencyProperty.RegisterAttached("IsEnabled", typeof(bool), typeof(ListBoxRightClickSelectionBehavior), new PropertyMetadata(false, OnIsEnabledChanged));
+
+        public static bool GetIsEnabled(DependencyObject dependencyObject) => (bool)dependencyObject.GetValue(IsEnabledProperty);
+
+        public static void SetIsEnabled(DependencyObject dependencyObject, bool value) => dependencyObject.SetValue(IsEnabledProperty, value);
+
+        private static T? FindParent<T>(DependencyObject? dependencyObject) where T : DependencyObject
+        {
+            while (dependencyObject != null)
+            {
+                if (dependencyObject is T typedParent)
+                {
+                    return typedParent;
+                }
+
+                dependencyObject = VisualTreeHelper.GetParent(dependencyObject);
+            }
+
+            return null;
+        }
+
+        private static void OnIsEnabledChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs e)
+        {
+            if (dependencyObject is not ListBox listBox)
+            {
+                return;
+            }
+
+            if ((bool)e.NewValue)
+            {
+                listBox.PreviewMouseRightButtonDown += OnPreviewMouseRightButtonDown;
+                return;
+            }
+
+            listBox.PreviewMouseRightButtonDown -= OnPreviewMouseRightButtonDown;
+        }
+
+        private static void OnPreviewMouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is not ListBox listBox)
+            {
+                return;
+            }
+
+            var item = FindParent<ListBoxItem>(e.OriginalSource as DependencyObject);
+
+            if (item == null)
+            {
+                return;
+            }
+
+            if (!item.IsSelected)
+            {
+                listBox.SelectedItems.Clear();
+                item.IsSelected = true;
+            }
+
+            item.Focus();
+        }
+    }
+}
